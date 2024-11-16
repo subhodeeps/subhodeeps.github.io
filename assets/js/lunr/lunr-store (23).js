54742b010bf8a58512698e5b8aa5d722e609d7c3
subhodeeps <subhodeep.sarkar1@gmail.com>
2024-11-17 02:26:09 +0530

    deleted photos

---
+++ assets/js/lunr/lunr-store (23).js
@@ -0,0 +1,175 @@
+var store = [{
+        "title": "Genius: The Life and Science of Richard Feynman",
+        "excerpt":"I love this book. It’s a priceless gem and one day I will put down in paper all that I want to say about this biography. But today is not that day. I will however take this space to direct your attention to a few articles that I had read....","categories": [],
+        "tags": [],
+        "url": "/books/genius/",
+        "teaser": null
+      },{
+        "title": "Infinite Jest",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/books/infinite-jest/",
+        "teaser": null
+      },{
+        "title": "Crime and Punishment",
+        "excerpt":"Heading      item 1   item 2   \\[E = mc^2\\] ","categories": [],
+        "tags": [],
+        "url": "/books/crime-and-punishment/",
+        "teaser": null
+      },{
+        "title": "India After Gandhi: The History of the World's Largest Democracy",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/books/india-after-gandhi/",
+        "teaser": null
+      },{
+        "title": "Consider the Lobster and Other Essays ",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/books/consider-the-lobster/",
+        "teaser": null
+      },{
+        "title": "Kafka on the Shore",
+        "excerpt":"We are feeble creatures made up of memories and experiences. What are we without them? Empty shells perhaps. Hollow men. How strong is our desire to relive certain sensations over and over again? Wouldn’t it be great if we could pull ourselves out of our fixed coordinates in this mechanistic...","categories": [],
+        "tags": [],
+        "url": "/books/kafka-on-the-shore/",
+        "teaser": null
+      },{
+        "title": "Gravity's Rainbow",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/books/gravity-s-rainbow/",
+        "teaser": null
+      },{
+        "title": "Capital in the Twenty-First Century",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/books/capital-in-the-21st-century/",
+        "teaser": null
+      },{
+        "title": "Sea of Tranquility: A novel",
+        "excerpt":"We might reasonably think about the end of the world as a continuous and never-ending process. It takes nothing short of a genius to mix old science fiction clichés like time travel and the simulation hypothesis to produce a novel that is able to closely examine the human condition in...","categories": [],
+        "tags": [],
+        "url": "/books/sea-of-tranquility/",
+        "teaser": null
+      },{
+        "title": "A Certain Hunger",
+        "excerpt":"I could never be a mass murderer. Mass murder is gauche. Mass murder is to serial killing as McDonald’s is to Peter Luger. Both establishments serve chopped beef, but one is indiscriminate to the point of ubiquity whereas the other is carnal dining at its bespoke finest. Should I call...","categories": [],
+        "tags": [],
+        "url": "/books/a-certain-hunger/",
+        "teaser": null
+      },{
+        "title": "Dance Dance Dance",
+        "excerpt":"“My peak? Would I even have one? I hardly had had anything you could call a life. A few ripples. some rises and falls. But that’s it. Almost nothing. Nothing born of nothing. I’d loved and been loved, but I had nothing to show. It was a singularly plain, featureless...","categories": [],
+        "tags": [],
+        "url": "/books/dance-dance-dance/",
+        "teaser": null
+      },{
+        "title": "The Secret History",
+        "excerpt":"Still Reading It’s a very Greek idea, and a very profound one. Beauty is terror. Whatever we call beautiful, we quiver before it. And what could be more terrifying and beautiful, to souls like the Greeks or our own, than to lose control completely? To throw off the chains of...","categories": [],
+        "tags": [],
+        "url": "/books/the-secret-history/",
+        "teaser": null
+      },{
+        "title": "The Emperor's New Mind: Concerning Computers, Minds and The Laws of Physics",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/books/emperor-snew-mind/",
+        "teaser": null
+      },{
+        "title": "Gödel, Escher, Bach: an Eternal Golden Braid",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/books/geb/",
+        "teaser": null
+      },{
+        "title": "The Emperor of All Maladies: A Biography of Cancer",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/books/emperor-of-all-maladies/",
+        "teaser": null
+      },{
+        "title": "Consciousness Explained",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/books/consciousness-explained/",
+        "teaser": null
+      },{
+        "title": "The Last Mughal: The Fall of a Dynasty, Delhi 1857",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/books/the-last-mughal/",
+        "teaser": null
+      },{
+        "title": "Resolve",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/books/resolve/",
+        "teaser": null
+      },{
+        "title": "The People in the Trees",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/books/people-in-the-trees/",
+        "teaser": null
+      },{
+        "title": "The Sleepwalkers",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/books/the-sleepwalkers/",
+        "teaser": null
+      },{
+        "title": "Tomb of Sand",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/books/tomb-of-sand/",
+        "teaser": null
+      },{
+        "title": "Breast and Eggs",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/books/breats-and-eggs/",
+        "teaser": null
+      },{
+        "title": "Portfolio item number 1",
+        "excerpt":"This is an item in your portfolio. It can be have images or nice text. If you name the file .md, it will be parsed as markdown. If you name the file .html, it will be parsed as HTML.  ","categories": [],
+        "tags": [],
+        "url": "/portfolio/portfolio-1/",
+        "teaser": null
+      },{
+        "title": "Portfolio item number 2",
+        "excerpt":"This is an item in your portfolio. It can be have images or nice text. If you name the file .md, it will be parsed as markdown. If you name the file .html, it will be parsed as HTML.  ","categories": [],
+        "tags": [],
+        "url": "/portfolio/portfolio-2/",
+        "teaser": null
+      },{
+        "title": "The phase portrait of a nonlinear pendulum.",
+        "excerpt":"DRAFT VERSION: Using this as an excuse to test the blog! The motion of a nonlinear pendulum is governed by the well known equation, \\[\\begin{align} \\dfrac{d^2 x}{d t^2} + \\sin x = 0 . \\end{align}\\] The above equation is an example of a second-order, nonlinear ordinary differential equation (ODE). We...","categories": [],
+        "tags": ["nonlinear dynamics","phase portrait","classical mechanics"],
+        "url": "/posts/2022/10/nonlinear_dynamics/",
+        "teaser": null
+      },{
+        "title": "Some articles, essays, etc. that I've enjoyed reading",
+        "excerpt":"         The man who invented black holes, Gary Gibbons (New Scientist, 26 June 1979).            Wide hats and narrow minds, Stephen Jay Gould (New Scientist 8 March 1979, p. 777. Reprinted in The Panda&#8217;s Thumb, p. 145)      ","categories": [],
+        "tags": [],
+        "url": "/teaching/articles/",
+        "teaser": null
+      },{
+        "title": "Teaching experience 2",
+        "excerpt":"This is a description of a teaching experience. You can use markdown like any other post.   Heading 1   Heading 2   Heading 3  ","categories": [],
+        "tags": [],
+        "url": "/teaching/2015-spring-teaching-1",
+        "teaser": null
+      },{
+        "title": "Links to some good lecture notes and other useful online resources",
+        "excerpt":"The internet is a really cool place when it comes to learning literally anything and everything, provided you know where to find the right pdf file! That’s why I have decided to keep a page where I can curate some of the lecture notes that I’ve enjoyed studying. Moreover, the...","categories": [],
+        "tags": [],
+        "url": "/teaching/onlineres/",
+        "teaser": null
+      },{
+        "title": "How To Be the Best Version of Yourself",
+        "excerpt":"This page has been adapted from the Essential Skills section of the Grittings website. It echoes and outlines a certain credo I am trying to live by, and I include it since it is exactly that advice I would love to give to my younger self. Work. Work. Work. Do...","categories": [],
+        "tags": [],
+        "url": "/teaching/workethics/",
+        "teaser": null
+      }]
